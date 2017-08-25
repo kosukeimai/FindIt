e68fb66621b579164c01bac4507551615083dd9b
Naoki Egami <naoki-egami@users.noreply.github.com>
2017-08-25 02:10:01 -0400

    Ver1.1.1

--- R/test.CausalANOVA.R
+++ R/test.CausalANOVA.R
@@ -31,6 +31,41 @@
 #' 
 #' Post, J. B. and Bondell, H. D. 2013. ``Factor selection and structural
 #' identification in the interaction anova model.'' Biometrics 69, 1, 70--79.
+#' @examples
+#' ## ####################################### 
+#' ## With Screening and Collapsing
+#' ## #######################################
+#' data(Carlson)
+#' ## Specify the order of each factor
+#' Carlson$newRecordF<- factor(Carlson$newRecordF,ordered=TRUE,
+#'                             levels=c("YesLC", "YesDis","YesMP",
+#'                                      "noLC","noDis","noMP","noBusi"))
+#' Carlson$promise <- factor(Carlson$promise,ordered=TRUE,levels=c("jobs","clinic","education"))
+#' Carlson$coeth_voting <- factor(Carlson$coeth_voting,ordered=FALSE,levels=c("0","1"))
+#' Carlson$relevantdegree <- factor(Carlson$relevantdegree,ordered=FALSE,levels=c("0","1"))
+#'
+#' ## Sample Splitting
+#' train.ind <- sample(unique(Carlson$respcodeS), 272, replace=FALSE)
+#' test.ind <- setdiff(unique(Carlson$respcodeS), train.ind)
+#' Carlson.train <- Carlson[is.element(Carlson$respcodeS,train.ind), ]
+#' Carlson.test <- Carlson[is.element(Carlson$respcodeS,test.ind), ]
+#'  
+#' #################### AMEs and two-way AMIEs ####################
+#' fit.r2 <- CausalANOVA(formula=won ~ newRecordF + promise + coeth_voting + relevantdegree,
+#'                       data=Carlson.train, pair.id=Carlson.train$contestresp,diff=TRUE,
+#'                       screen=TRUE, collapse=TRUE,
+#'                       cluster=Carlson.train$respcodeS, nway=2)
+#' summary(fit.r2)
+#' 
+#' ## refit with test.CausalANOVA
+#' fit.r2.new <- test.CausalANOVA(fit.r2, newdata=Carlson.test, diff=TRUE,
+#'                                pair.id=Carlson.test$contestresp, cluster=Carlson.test$respcodeS)
+#' 
+#' summary(fit.r2.new)
+#' plot(fit.r2.new)
+#' plot(fit.r2.new, type="ConditionalEffect", fac.name=c("newRecordF","coeth_voting"))
+#' ConditionalEffect(fit.r2.new, treat.fac="newRecordF", cond.fac="coeth_voting")
+
 test.CausalANOVA <- function(fit, newdata, collapse.level=TRUE, diff=FALSE, pair.id=NULL,cluster=NULL){
     ## I need to install newdata as well as new information about pair.id
     
