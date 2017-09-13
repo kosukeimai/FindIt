b59411fa86592a7924c94b45a5ba17317e4bf8ee
HJ08003 <hubertj@princeton.edu>
2017-09-13 12:53:15 -0400

    devtools::use_testthat()  and added test-all.R

---
+++ tests/testthat/test-all.R
@@ -0,0 +1,14 @@
+rm(list=ls())
+library(FindIt)
+library(testthat)
+context("tests FindIt")
+
+
+# set random seed
+set.seed(12345)
+
+test_that("tests FindIt on XYZ data", {
+  ## load the data
+})  
+  
+
