3b2b02f14274c7722e29702beb6bac8bf80c5b6b
naoki-egami <negami@princeton.edu>
2019-11-18 23:37:06 -0500

    up R project

--- R/Glsei_old.R
+++ R/Glsei_old.R
@@ -7,7 +7,7 @@
 
 Glsei <- function (A = NULL, B = NULL, E = NULL, F = NULL, G = NULL, H = NULL, 
                    Wx = NULL, Wa = NULL, type = 1, tol = sqrt(.Machine$double.eps), 
-                   tolrank = NULL, fulloutput = FALSE, verbose = TRUE) 
+                   tolrank = NULL, fulloutput = FALSE, verbose = TRUE, PACKAGE="FindIt") 
 {
   if (is.vector(E) & length(F) == 1) 
     E <- t(E)
@@ -80,7 +80,72 @@ Glsei <- function (A = NULL, B = NULL, E = NULL, F = NULL, G = NULL, H = NULL,
   if (is.null(Tol)) 
     Tol <- sqrt(.Machine$double.eps)
   IsError <- FALSE
-  if (type == 2) {
+  if (type == 1) {
+    ineq <- Nin + Nx
+    mIP <- ineq + 2 * Nx + 2
+    lpr <- 1
+    if (fulloutput) 
+      lpr <- lpr + 3
+    if (!is.null(tolrank)) 
+      lpr <- lpr + 6
+    if (!is.null(Wx)) {
+      lw <- length(Wx)
+      lpr <- lpr + 2 + lw
+    }
+    ProgOpt <- rep(1, lpr)
+    if (lpr > 1) {
+      ipr <- 1
+      if (fulloutput) {
+        ProgOpt[ipr:(ipr + 2)] <- c(ipr + 3, 1, 1)
+        ipr <- ipr + 3
+      }
+      if (!is.null(tolrank)) {
+        if (length(tolrank) == 1) 
+          tolrank <- rep(tolrank, len = 2)
+        ProgOpt[ipr:(ipr + 5)] <- c(ipr + 6, 4, tolrank[1], 
+                                    ipr + 6, 5, tolrank[2])
+        ipr <- ipr + 6
+      }
+      if (!is.null(Wx)) {
+        lw <- length(Wx)
+        if (lw == 1) {
+          ProgOpt[ipr:(ipr + 2)] <- c(ipr + 3, 2, 1)
+        }
+        else {
+          if (lw != Nx) 
+            stop("cannot solve least squares problem - number of weighs should be =1 or =number of unknowns")
+          lw <- lw + ipr + 1
+          ProgOpt[ipr:lw] <- c(lw + 1, 3, Wx)
+        }
+      }
+    }
+    mdW <- Neq + Napp + ineq
+    if (fulloutput) 
+      mdW <- max(mdW, Nx)
+    mWS <- 2 * (Neq + Nx) + max(Napp + ineq, Nx) + (ineq + 
+                                                      2) * (Nx + 7)
+    storage.mode(A) <- storage.mode(B) <- "double"
+    storage.mode(E) <- storage.mode(F) <- "double"
+    storage.mode(G) <- storage.mode(H) <- "double"
+    sol <- .Fortran("lsei", NUnknowns = Nx, NEquations = Neq, 
+                    NConstraints = Nin, NApproximate = Napp, A = A, B = B, 
+                    E = E, F = F, G = G, H = H, X = as.vector(rep(0, 
+                                                                  Nx)), mIP = as.integer(mIP), mdW = as.integer(mdW), 
+                    mWS = as.integer(mWS), IP = as.integer(rep(0, mIP)), 
+                    W = as.double(matrix(data = 0, nrow = mdW, ncol = Nx + 
+                                           1)), WS = as.double(rep(0, mWS)), lpr = as.integer(lpr), 
+                    ProgOpt = as.double(ProgOpt), verbose = as.logical(verbose), 
+                    IsError = as.logical(IsError),PACKAGE="FindIt")
+    if (any(is.infinite(sol$nX))) 
+      sol$IsError <- TRUE
+    if (fulloutput) {
+      covar <- matrix(data = sol$W, nrow = mdW, ncol = Nx + 
+                        1)[1:Nx, 1:Nx]
+      RankEq <- sol$IP[1]
+      RankApp <- sol$IP[2]
+    }
+  }
+  else if (type == 2) {
     if (!is.null(Wx)) 
       stop("cannot solve least squares problem - weights not implemented for type 2")
     if (!is.null(Wa)) 
