e2cfba33187fe2d3013e99c416e6a5075ef06da7
naoki-egami <negami@princeton.edu>
2019-11-19 16:12:21 -0500

    up (ignore internal example)

--- example/example_CausalANOVA.R
+++ example/example_CausalANOVA.R
@@ -14,15 +14,14 @@ Carlson$relevantdegree <- factor(Carlson$relevantdegree,ordered=FALSE,levels=c("
 fit1 <- CausalANOVA(formula=won ~ newRecordF + promise + coeth_voting + relevantdegree,
                     data=Carlson, pair.id=Carlson$contestresp, diff=TRUE,
                     cluster=Carlson$respcodeS, nway=1)
-summary(fit1)
-plot(fit1, add  =  TRUE)
+summary(fit1) ## OK
 
 #################### AMEs and two-way AMIEs ####################
 fit2 <- CausalANOVA(formula=won ~ newRecordF + promise + coeth_voting + relevantdegree,
                     int2.formula = ~ newRecordF:coeth_voting,
                     data=Carlson, pair.id=Carlson$contestresp,diff=TRUE,
                     cluster=Carlson$respcodeS, nway=2)
-summary(fit2)
+summary(fit2) ## OK 
 plot(fit2, type="ConditionalEffect", fac.name=c("newRecordF", "coeth_voting"))
 ConditionalEffect(fit2, treat.fac="newRecordF", cond.fac="coeth_voting")
 
@@ -35,7 +34,7 @@ fit3 <- CausalANOVA(formula=won ~ newRecordF + promise + coeth_voting + relevant
                     int3.formula = ~ newRecordF:promise:coeth_voting,
                     data=Carlson, pair.id=Carlson$contestresp,diff=TRUE,
                     cluster=Carlson$respcodeS, nway=3)
-summary(fit3)
+summary(fit3) ## OK
 plot(fit3, type="AMIE", fac.name=c("newRecordF","promise", "coeth_voting"),space=25,adj.p=2.2)
 }
 
@@ -43,7 +42,7 @@ plot(fit3, type="AMIE", fac.name=c("newRecordF","promise", "coeth_voting"),space
 ## With Screening and Collapsing
 ## #######################################
 ## Sample Splitting
-train.ind <- sample(unique(Carlson$respcodeS), 272, replace=FALSE)
+train.ind <- unique(Carlson$respcodeS)[1:272]
 test.ind <- setdiff(unique(Carlson$respcodeS), train.ind)
 Carlson.train <- Carlson[is.element(Carlson$respcodeS,train.ind), ]
 Carlson.test <- Carlson[is.element(Carlson$respcodeS,test.ind), ]
@@ -51,15 +50,15 @@ Carlson.test <- Carlson[is.element(Carlson$respcodeS,test.ind), ]
 #################### AMEs and two-way AMIEs ####################
 fit.r2 <- CausalANOVA(formula=won ~ newRecordF + promise + coeth_voting + relevantdegree,
                       data=Carlson.train, pair.id=Carlson.train$contestresp,diff=TRUE,
-                      screen=TRUE, collapse=TRUE,
+                      screen = TRUE, collapse = TRUE,
                       cluster=Carlson.train$respcodeS, nway=2)
-summary(fit.r2)
+summary(fit.r2) ## OK 
 
 ## refit with test.CausalANOVA
 fit.r2.new <- test.CausalANOVA(fit.r2, newdata=Carlson.test, diff=TRUE,
                                pair.id=Carlson.test$contestresp, cluster=Carlson.test$respcodeS)
 
-summary(fit.r2.new)
+summary(fit.r2.new) ## OK 
 plot(fit.r2.new)
 plot(fit.r2.new, type="ConditionalEffect", fac.name=c("newRecordF","coeth_voting"))
 ConditionalEffect(fit.r2.new, treat.fac="newRecordF", cond.fac="coeth_voting")
